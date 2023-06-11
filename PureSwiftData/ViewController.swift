201464823c3b402488c2db1904f63ae6cf82c176
Thomas <thomas@halbritter.org>
2023-06-11 13:59:34 +0200

    Initial Commit

---
+++ PureSwiftData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PureSwiftData
+//
+//  Created by Thomas on 11.06.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
